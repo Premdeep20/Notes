a23cee506c691f8e7d4762bbcfe0ec1dfa013a02
PremdeepNL <premdeep@nineleaps.com>
2021-02-05 04:05:14 +0530

    oninput for title and description

---
+++ Notes.js
@@ -0,0 +1,7 @@
+function changeTitle(e){
+    document.getElementById('title').value = e.target.value;
+}
+
+function changeDescription(e){
+    document.getElementById('description').value = e.target.value;
+}
